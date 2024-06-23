06ab762caefdc107127f9b784e3bccc1889641b2
KrisshDiwedy38 <krisshdiwedy38@gmail.com>
2024-06-23 06:24:24 +0530

    Add files via upload

---
+++ INTERSHIP/Calculator/calc.js
@@ -0,0 +1,30 @@
+let input = document.getElementById('inputBox')
+let buttons = document.querySelectorAll('button');
+
+let string = "";
+let arr = Array.from(buttons);
+console.log(arr);
+arr.forEach(buttons => 
+   {
+      buttons.addEventListener('click' , (click) => 
+         {
+            if (click.target.innerText === "="){
+               string = eval(string)
+               input.value = string;
+            }
+            else if(click.target.innerText === "AC"){
+               string = "";
+               input.value = string;
+            }
+            else if(click.target.innerText === "C"){
+               string = string.substring(0 , string.length-1);
+               input.value=string;
+            }
+            else{
+            string += click.target.innerText;
+            input.value = string;
+            };
+         
+      })
+   }
+)
